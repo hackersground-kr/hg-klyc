a2a8a4900822fdb4311842aad07bdd4997536a94
김시원 <123472978+ENCAIL@users.noreply.github.com>
2024-08-27 03:11:55 +0900

    Delete HgProject directory

--- HgProject/src/main/java/org/example/hgproject/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.hgproject.comment.repository;
-
-import org.example.hgproject.comment.entity.CommentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<CommentEntity,Long> {
-}
