d80d53fff781c55d652d8b1ade2d6bd1932efe0c
lyhtll <166387914+lyhtll@users.noreply.github.com>
2024-08-26 23:53:35 +0900

    Update JoinService.java

--- HgProject/src/main/java/org/example/hgproject/user/service/JoinService.java
+++ HgProject/src/main/java/org/example/hgproject/user/service/JoinService.java
@@ -31,7 +31,7 @@ public class JoinService {
 
         data.setUserName(userName);
         data.setPassWord(bCryptPasswordEncoder.encode(passWord));
-        data.setRole("ROLE_ADMIN");
+        data.setRole("ROLE_USER");
 
         userRepository.save(data);
     }
