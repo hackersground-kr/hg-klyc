aa4ee3d1e9287a2a8bdf88b1b14b15f0d5e6f44b
김시원 <123472978+ENCAIL@users.noreply.github.com>
2024-08-26 15:49:46 +0900

    server

---
+++ HtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k.ht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
