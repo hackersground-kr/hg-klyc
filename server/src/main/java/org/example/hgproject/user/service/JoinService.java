602411e694c4cd728fcef23a1359712532ce8d1f
김시원 <siwon0876@gmail.com>
2024-08-27 03:28:38 +0900

    server

--- server/src/main/java/org/example/hgproject/user/service/JoinService.java
+++ server/src/main/java/org/example/hgproject/user/service/JoinService.java
@@ -23,15 +23,15 @@ public class JoinService {
 
         Boolean isExist = userRepository.existsByUserName(userName);
 
-        if(isExist){
-            return;
+        if (userRepository.existsByUserName(userName)) {
+            throw new IllegalArgumentException("이미 사용 중인 사용자 이름입니다.");
         }
 
         UserEntity data = new UserEntity();
 
         data.setUserName(userName);
         data.setPassWord(bCryptPasswordEncoder.encode(passWord));
-        data.setRole("ROLE_ADMIN");
+        data.setRole("ROLE_USER");
 
         userRepository.save(data);
     }
